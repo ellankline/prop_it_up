a5ef326bf79cc3db7d711e2a640694547e93bf9c
Elle Kline <ellankline@gmail.com>
2021-02-16 09:38:07 -0800

    added button to increment each person's age

--- prop_it_up/src/components/PersonCard.js
+++ prop_it_up/src/components/PersonCard.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-    
-    
 class PersonCard extends Component {
-    render() {
-    const {firstName, lastName, age, hairColor} = this.props;
-    return (
-        <div>
-            <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
-            <p>Hair Color: {hairColor}</p>
-        </div>
-    )
-    }
+            constructor(props) {
+                super(props);
+                this.state = {
+                    clicks:0,
+                    show:true,
+                };
+        };
+                
+        IncrementItem = () => {
+            this.setState({ clicks: this.state.clicks + 1 });
+        };
+        
+        render() {
+        const { firstName, lastName, age, hairColor } = this.props;
+        return (
+            <div>
+                <h1>{lastName}, {firstName}</h1>
+                <p>Age: {age + this.state.clicks}</p>
+                <p>Hair Color: {hairColor}</p>
+                <button onClick={this.IncrementItem}>Birthday Button for {firstName} {lastName}</button>
+            </div>
+        );
+    };
 }
-    
+
+
 export default PersonCard;
